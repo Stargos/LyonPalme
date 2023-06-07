c6d908fe491552cda57749cd4fc4733cc86d6566
tom <tomperrat@gmail.com>
2023-06-07 16:26:37 +0200

    règlement de conflits de migrations

--- lyon_palme/inscription/urls.py
+++ lyon_palme/inscription/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('login_nageur', views.login_nageur, name='login_nageur'),
     path('login_secretaire', views.login_secretaire, name='login_secretaire'),
     path('change_password', views.change_password, name='change_password'),
-    path('accueil_secretaire', views.AccueilSecretaire, name='AccueilSecretaire'),
+    path('accueil_secretaire', views.accueil_secretaire, name='accueil_secretaire'),
     path('accueil_secretaire/<int:adherent_id>/', views.nageur, name="nageur"),
     path('accueil_secretaire/<int:adherent_id>/modification_nageur',views.modification_nageur, name="modification_nageur"),
-    path('accueil_secretaire', views.AccueilSecretaire, name='accueil_secretaire'),
-    path('accueil_nageur', views.AccueilNageur, name='accueil_nageur'),
+    path('accueil_secretaire', views.accueil_secretaire, name='accueil_secretaire'),
+    path('accueil_nageur', views.accueil_nageur, name='accueil_nageur'),
     path('', views.Accueil, name="Accueil")
 ]
 
